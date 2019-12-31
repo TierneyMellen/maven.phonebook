f8b6b412c9ce0a7076a73ed03de866b893c4db85
unknown <tierney_mellen@yahoo.com>
2019-12-30 23:09:56 -0500

    First commit. Add and remove in the simple phonebook work. Some kinks in getAllContactNames. Reverse lookup is gonna be a pain

--- src/main/java/com/github/curriculeon/SimplePhoneBook.java
+++ src/main/java/com/github/curriculeon/SimplePhoneBook.java
@@ -1,32 +1,52 @@
 package com.github.curriculeon;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by leon on 12/16/2019.
  */
 public class SimplePhoneBook {
+
+    private Map<String, String> simplePhoneBook = new HashMap<>();
+
     public void add(String name, String phoneNumberToAdd) {
 
-    }
+        this.simplePhoneBook.put(name, phoneNumberToAdd);
 
-    public String get(String name) {
-        return null;
+    }
+    public void remove(String name) {
+
+        this.simplePhoneBook.remove(name);
+
     }
 
     public Boolean hasEntry(String phoneNumber) {
-        return null;
+
+        return this.simplePhoneBook.containsValue(phoneNumber);
+
     }
 
-    public void remove(String name) {
+    public String get(String name) { //lookup function
+
+        return this.simplePhoneBook.get(name);
+
+    }
+
+    public String reverseLookup(String phoneNumber) {
+        Map<String, String> tempMap = new HashMap<>();
+        Set<String> nameSet = this.simplePhoneBook.keySet();
+        Set <String> valueSet = (Set) this.simplePhoneBook.values();
+
+        return null;
 
     }
 
     public List<String> getAllContactNames() {
-        return null;
-    }
+        Set<String> nameSet = this.simplePhoneBook.keySet();
+        List<String> nameList = new ArrayList();
+        nameList.addAll(nameSet);
+        //Collections.sort(nameList);
+        return nameList;
 
-    public String reverseLookup(String phoneNumber) {
-        return null;
     }
 }
